2dd7d1d16004f057a7dc54f3bf21038dc3fd1c7a
Nan Khin Mhwe <nankhinmhwe56@gmail.com>
2019-07-29 18:24:29 +0630

    Implement UI frame with vuetify and Enable CORS in backend

---
+++ pages/login.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-form ref="form" v-model="valid" lazy-validation @submit.prevent="login">
+    <v-app id="login" class="primary">
+      <v-content>
+        <v-container fluid fill-height>
+          <v-layout align-center justify-center>
+
+            <v-flex xs12 sm8 md4 lg4>
+              <v-card class="elevation-1 pa-3">
+                <v-card-text>
+                  <div class="layout column align-center">
+                    <img src="/v.png" alt="Vue Material Admin"
+                         height="135">
+                    <h1 class="flex my-4 primary--text header-font">Sign In to Your Account</h1>
+                  </div>
+                  <span class="red--text" v-if="responseError">{{responseError}}</span>
+                  <v-text-field
+                    prepend-icon="email"
+                    ref="email"
+                    name="email"
+                    label="Email"
+                    type="email"
+                    v-model="user.email"
+                    :rules="rules.emailRules">
+                  </v-text-field>
+                  <v-text-field
+                    prepend-icon="lock"
+                    name="password"
+                    label="Password"
+                    id="password"
+                    type="password"
+                    v-model="user.password"
+                    :rules="rules.passwordRules">
+                  </v-text-field>
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn text medium color="primary" to="/register">Not yet register?</v-btn>
+                  <v-spacer></v-spacer>
+                  <v-btn type="submit" color="primary" :loading="loading" :disabled="!valid">Sign In</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-content>
+    </v-app>
+  </v-form>
+
+
+</template>
+
+<script>
+  import {LoginRules} from "../helpers/validation-rules";
+
+  export default {
+    layout: "clean",
+    name: "login",
+    middleware: 'guest',
+    data: () => ({
+      loading: false,
+      responseError: '',
+      valid: true,
+      rules: new LoginRules(),
+      user: {
+        email: '',
+        password: ''
+      }
+    }),
+    methods: {
+      async login() {
+        if (!this.$refs.form.validate()){
+          return;
+        }
+        this.loading = true;
+        try {
+          await this.$auth.loginWith('local', {
+            data: {user: this.user}
+          })
+
+        } catch (e) {
+          if (e.response.data.error) {
+            this.responseError = e.response.data.error;
+          } else {
+            this.responseError = e.response.data.errors;
+          }
+        } finally {
+          this.loading = false;
+          this.$router.push('/');
+          console.log('here')
+        }
+      },
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
