bb70c98c1951585fceeaa6142595d1e04d7b7f11
Nan Khin Mhwe <nankhinmhwe56@gmail.com>
2019-08-05 16:06:32 +0630

    added default axios url

--- web/plugins/axios.js
+++ web/plugins/axios.js
@@ -1,4 +1,5 @@
 export default function({$axios, store}, context){
+  $axios.defaults.baseURL = process.env.BASE_URL || 'http://localhost:3000';
   $axios.onRequest( config => {
     // added authorization header to subsequence http requests
     if (store.state.token) {
