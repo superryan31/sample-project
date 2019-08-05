12b281d17649b42dded1d8c2bf95d82deb922192
Nan Khin Mhwe <nankhinmhwe56@gmail.com>
2019-08-05 16:12:13 +0630

    rename baseUrl

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,5 @@
 export default function({$axios, store}, context){
-  $axios.defaults.baseURL = process.env.BASE_URL || 'http://localhost:3000';
+  $axios.defaults.baseURL = process.env.baseUrl || 'http://localhost:3000';
   $axios.onRequest( config => {
     // added authorization header to subsequence http requests
     if (store.state.token) {
