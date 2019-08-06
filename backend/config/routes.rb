d290f934106801723ce6c0684e56bc80b9abeacc
Nan Khin Mhwe <nankhinmhwe56@gmail.com>
2019-08-06 17:21:24 +0630

    Implement api for getting GitHub issue list

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :issues, only: [:index, :show] , defaults: { format: :json }
   devise_for :users, defaults: { format: :json }
+
+  #
+  # get 'issues' , defaults: { format: :json }
 end
